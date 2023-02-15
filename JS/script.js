e8a9f58a0feb708ecfe4aa08bdfce40483eab9f6
Paweł Czwarno <pawel.czwarno@gmail.com>
2023-02-15 20:34:21 +0100

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+console.log("Witaj na stronie domowej Pawła Czwarno!");
+
+let body = document.querySelector(".js-body");
+let themeName = document.querySelector(".js-themeName");
+let buttonTheme = document.querySelector(".js-navigation__buttonTheme");
+let buttonPhoto = document.querySelector(".js-article__buttonPhoto");
+let photo = document.querySelector(".js-article__photo");
+let photoToggle = document.querySelector(".js-photoToggle");
+
+buttonTheme.addEventListener("click", () => {
+    body.classList.toggle("body--dark");
+
+    themeName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
+});
+
+buttonPhoto.addEventListener("click", () => {
+    photo.classList.toggle("js-photoToggle");
+
+    buttonPhoto.innerText = photo.classList.contains("js-photoToggle") ? "Pokaż zdjęcie" : "Schowaj zdjęcie";
+});
